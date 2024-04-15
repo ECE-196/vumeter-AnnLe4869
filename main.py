24806661c5bae28b5d0219698ceed1d8636fbdf4
Anh Le <kunquan2345@gmail.com>
2024-04-15 16:33:45 -0700

    Add program that turn led based on mic input

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ led_pins = [
     board.IO26, # type: ignore
     board.IO47,
     # do the rest...
+    board.IO33,
+    board.IO34,
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
@@ -23,15 +31,27 @@ for led in leds:
 
 # main loop
 while True:
+    # the value range from 0 to 65535
+    # try to divide this into len(led_pins)
     volume = microphone.value
+    
+    MAX_VAL_CEIL = 65535
+    
+    jump_step = MAX_VAL_CEIL // len(led_pins)
 
     print(volume)
+    
+    leds_height = volume // jump_step
+    
+    
+    # first we turn all off
+    for x in range(0, len(led_pins)):
+        leds[x].value = 0
+    
+    # then turn on the leds that are in range
+    for x in range(0, leds_height):
+        leds[x].value = 1
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    
 
-    sleep(1)
-
-    # instead of blinking,
-    # how can you make the LEDs
-    # turn on like a volume meter?
+    # sleep(1)
